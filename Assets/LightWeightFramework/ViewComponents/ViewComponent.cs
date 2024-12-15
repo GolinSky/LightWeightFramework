c985f4abef68f9dbdfc78bbadd6fb796d7d8b03a
Golinsky <golinsky97@gmail.com>
2024-12-15 18:52:04 +0200

    - add abstraction init flow

--- Assets/LightWeightFramework/ViewComponents/ViewComponent.cs
+++ Assets/LightWeightFramework/ViewComponents/ViewComponent.cs
@@ -5,7 +5,21 @@ namespace LightWeightFramework.Components.ViewComponents
 {
     public abstract class ViewComponent:MonoBehaviour
     {
-        public virtual IModelObserver ModelObserver { get; private set; }
+        public virtual IModelObserver ModelObserver { get; protected set; }
+        
+        
+        public virtual void Init()
+        {
+            OnInit();
+        }
+        
+        public virtual void Dispose()
+        {
+            OnRelease();
+        }
+
+        protected virtual void OnInit(){}
+        protected virtual void OnRelease(){}
     }
 
 
